e9ddf386be26b8624239b8f46f96601e71e9ec0d
DartSams <dartagnansams1@icloud.com>
2021-04-07 12:34:05 -0400

    added in the draw function

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-import time
 
 pygame.init()
 width,height=500,490
@@ -10,6 +9,7 @@ text=pygame.font.SysFont('comicsans',40)
 bg=(0,0,0)
 white=(255,255,255)
 black=(0,0,0)
+
 
 def draw_squares():
     global first,second,third,fourth,fifth,sixth,seventh,eigth,ninth
@@ -26,6 +26,7 @@ def draw_squares():
     ninth=pygame.draw.rect(screen,white,(350,350,100,100))
 
 draw_squares()
+# squares_lst=[first,second,third,fourth,fifth,sixth,seventh,eigth,ninth]
 
 class squares:
     def __init__(self,square,state):
@@ -46,7 +47,7 @@ squares_lst=[first_open,second_open,third_open,fourth_open,fifth_open,sixth_open
 
 
 def win_screen(player):
-    current_player=text.render(f"{player} wins" ,1,(255,255,255))
+    current_player=text.render(f"{player}" ,1,(255,255,255))
     screen.blit(current_player,(width//2-current_player.get_width()//2,height-current_player.get_height()))
     pygame.display.update()
     pygame.time.delay(2000)
@@ -58,8 +59,12 @@ def win_screen(player):
 
     
 
+
 def main():
     player1=True
+    player1_points=0
+    player2_points=0
+    # current_player=pygame.draw.circle(screen,(255,0,0),(15,15),10,8)
     pygame.draw.rect(screen,black,(0,0,width,50))
     pygame.display.update()
     current_player='X'
@@ -69,17 +74,22 @@ def main():
     while True:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
-                for i in squares_lst:
-                    print(i.square,i.state)
+                # for i in squares_lst:
+                #     print(i.square,i.state)
+                # print(player1_points)
+                # print(player2_points)
 
                 pygame.quit()
                 sys.exit()
 
             if event.type==pygame.MOUSEBUTTONUP:
                 pos=pygame.mouse.get_pos()
+                # print(pos)
                 if player1==True:
                     for i in squares_lst:
                         if i.square.collidepoint(pos):
+                            # print(i)
+                            # print(i.square[0],i.square[1],i.square[2],i.square[3])
                             pygame.draw.line(screen,(255,0,0),(i.square[0]+5,i.square[1]+5),(i.square[0]+i.square[2]-5,i.square[1]+i.square[2]-5),8)
                             pygame.draw.line(screen,(255,0,0),(i.square[0]+i.square[2]-5,i.square[1]+5),(i.square[0]+5,i.square[1]+i.square[2]-5),8)
                             i.state='Player1'
@@ -101,9 +111,11 @@ def main():
                     pygame.draw.rect(screen,black,(0,0,width,50))
                     pygame.display.update()
                     
+                    # current_player=pygame.draw.circle(screen,(255,0,0),(15,15),10,8)
                     current_player='X'
                     current_player=text.render(f"{current_player} turn" ,1,(255,255,255))
                     screen.blit(current_player,(10,2))
+                    player1_points+=1
 
 
                 else:
@@ -111,65 +123,69 @@ def main():
                     pygame.draw.rect(screen,black,(0,0,width,50))
                     pygame.display.update()
                     
+                    # current_player=pygame.draw.circle(screen,(0,0,255),(15,15),10,8)
                     current_player='O'
                     current_player=text.render(f"{current_player} turn" ,1,(255,255,255))
                     screen.blit(current_player,(10,2))
+                    player2_points+=1
 
                 
         ##check for player 1 win
         if first_open.state=='Player1' and second_open.state=='Player1' and third_open.state=='Player1':
-            win_screen('Player 1')
+            win_screen('Player 1 wins')
 
 
         elif fourth_open.state=='Player1' and fifth_open.state=='Player1' and sixth_open.state=='Player1':
-            win_screen('Player 1')
+            win_screen('Player 1 wins')
 
         elif seventh_open.state=='Player1' and eigth_open.state=='Player1' and ninth_open.state=='Player1':
-            win_screen('Player 1')
+            win_screen('Player 1 wins')
 
         elif first_open.state=='Player1' and fourth_open.state=='Player1' and seventh_open.state=='Player1':
-            win_screen('Player 1')
+            win_screen('Player 1 wins')
 
         elif second_open.state=='Player1' and fifth_open.state=='Player1' and eigth_open.state=='Player1':
-            win_screen('Player 1')
+            win_screen('Player 1 wins')
 
         elif third_open.state=='Player1' and sixth_open.state=='Player1' and ninth_open.state=='Player1':
-            win_screen('Player 1')
+            win_screen('Player 1 wins')
 
         elif first_open.state=='Player1' and fifth_open.state=='Player1' and ninth_open.state=='Player1':
-            win_screen('Player 1')
+            win_screen('Player 1 wins')
 
         elif third_open.state=='Player1' and fifth_open.state=='Player1' and seventh_open.state=='Player1':
-            win_screen('Player 1')
+            win_screen('Player 1 wins')
 
         
 
 
         ##Check for player 2 win
-        if first_open.state=='Player2' and second_open.state=='Player2' and third_open.state=='Player2':
-            win_screen('Player 2')
+        elif first_open.state=='Player2' and second_open.state=='Player2' and third_open.state=='Player2':
+            win_screen('Player 2 wins')
 
         elif fourth_open.state=='Player2' and fifth_open.state=='Player2' and sixth_open.state=='Player2':
-            win_screen('Player 2')
+            win_screen('Player 2 wins')
 
         elif seventh_open.state=='Player2' and eigth_open.state=='Player2' and ninth_open.state=='Player2':
-            win_screen('Player 2')
+            win_screen('Player 2 wins')
 
         elif first_open.state=='Player2' and fourth_open.state=='Player2' and seventh_open.state=='Player2':
-            win_screen('Player 2')
+            win_screen('Player 2 wins')
 
         elif second_open.state=='Player2' and fifth_open.state=='Player2' and eigth_open.state=='Player2':
-            win_screen('Player 2')
+            win_screen('Player 2 wins')
 
         elif third_open.state=='Player2' and sixth_open.state=='Player2' and ninth_open.state=='Player2':
-            win_screen('Player 2')
+            win_screen('Player 2 wins')
 
         elif first_open.state=='Player2' and fifth_open.state=='Player2' and ninth_open.state=='Player2':
-            win_screen('Player 2')
+            win_screen('Player 2 wins')
 
         elif third_open.state=='Player2' and fifth_open.state=='Player2' and seventh_open.state=='Player2':
-            win_screen('Player 2')
+            win_screen('Player 2 wins')
 
+        elif player1_points==4 and player2_points==5:
+            win_screen('Draw')
 
         pygame.display.update()
         FPS.tick(120)
